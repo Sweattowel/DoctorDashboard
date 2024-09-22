843bda93cda817e48f714a67e93e67bf47d0958a
Sweattowel <122240183+Sweattowel@users.noreply.github.com>
2024-09-22 23:07:34 +1000

    Test

--- Server/Server.cjs
+++ Server/Server.cjs
@@ -35,7 +35,7 @@ const port = 3001
 const corsOptions = {
     origin: "http://localhost:3000",
     optionsSuccessStatus: 200,
-    Credentials: true,
+    "Access-Control-Allow-Credentials": true
   };
 app.use(cors(corsOptions));
 
