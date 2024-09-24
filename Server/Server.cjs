aabf16cf9b9ea4dae2fcbc32d9d9c423356118e7
Sweattowel <122240183+Sweattowel@users.noreply.github.com>
2024-09-24 13:30:49 +1000

    Changes

--- Server/Server.cjs
+++ Server/Server.cjs
@@ -199,7 +199,7 @@ app.get("/api/testCookie", function (req, res) {
 app.post("/api/Authorization/RefreshToken", function (req, res) {
     try {
         console.log("Refreshing Cookie");
-        const data = req.userData;
+        const data = req.body;
         const cookie = req.cookies["Authorization"];
 
         if (!cookie) {
