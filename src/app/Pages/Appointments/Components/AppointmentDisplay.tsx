dcfa5fe6c83c8ee4b7f402d7c5664daca14c01b9
Sweattowel <122240183+Sweattowel@users.noreply.github.com>
2024-10-08 14:25:04 +1000

    Storm prep

--- src/app/Pages/Appointments/Components/AppointmentDisplay.tsx
+++ src/app/Pages/Appointments/Components/AppointmentDisplay.tsx
@@ -2,7 +2,7 @@
 
 import { userContext } from "@/app/Context/ContextProvider";
 import API from "@/app/Interceptor"
-import { AwaitedReactNode, JSXElementConstructor, Key, ReactElement, ReactNode, ReactPortal, useEffect, useState } from "react"
+import { AwaitedReactNode, FormEvent, JSXElementConstructor, Key, ReactElement, ReactNode, ReactPortal, useEffect, useState } from "react"
 
 // Types for Appointments
 interface Appointment {
@@ -295,17 +295,180 @@ interface ExpansionProps {
 }
 
 const Expansion = ({ data }: ExpansionProps) => {
+  const [ wantToAdjust, setWantToAdjust ] = useState<boolean>(false);
+  const [ error, setError ] = useState<string>("");
+  const [ loading, setLoading ] = useState<boolean>(false);
+
+  const [formData, setFormData] = useState<Appointment>({
+    ...data
+  })
+  
+  function HandleAdjustAppointment(e: FormEvent<HTMLFormElement>){
+    try {
+      e.preventDefault();
+      setLoading(true);
+
+
+
+    } catch (error) {
+      setLoading(false);
+      setError("Failed to adjust, please refresh"); 
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  if (loading){
+    return (
+      <section className="p-5 border rounded-2xl overflow-hidden transition-all duration-500 ease-in-out flex flex-col justify-evenly w-full mt-10 animate-pulse text-red-600 text-center">
+        Loading...
+      </section>
+    )
+  }
+  if (wantToAdjust){
+    return (
+      <form onSubmit={(e) => HandleAdjustAppointment(e)} className="p-5 border rounded-2xl overflow-hidden transition-all duration-500 ease-in-out flex flex-col justify-evenly w-full mt-10">
+        <section className="text-lg font-bold flex">
+        <div className="flex flex-row justify-between items-center w-[40%]">
+              <label className="flex justify-evenly w-full">
+                  <input
+                      type="radio"
+                      name="title"
+                      value="Mr"
+                      onChange={(e) => {
+                          setFormData((prevData) => ({
+                              ...prevData,
+                              Title: e.target.value
+                          }));
+                      }}
+                  />
+                  Mr
+              </label>
+              <label className="flex justify-evenly w-full">
+                  <input
+                      type="radio"
+                      name="title"
+                      value="Mrs"
+                      onChange={(e) => {
+                          setFormData((prevData) => ({
+                              ...prevData,
+                              Title: e.target.value
+                          }));
+                      }}
+                  />
+                  Mrs
+              </label>
+              <label className="flex justify-evenly w-full">
+                  <input
+                      type="radio"
+                      name="title"
+                      value="Mrs"
+                      onChange={(e) => {
+                          setFormData((prevData) => ({
+                              ...prevData,
+                              Title: e.target.value
+                          }));
+                      }}
+                  />
+                  Else
+              </label>
+          </div>
+          <input type="text" value={formData.ClientName} placeholder={data.ClientName} />
+        </section>
+        <label className="font-bold">Client Name:</label>
+        <input value={formData.Issue} placeholder={`${formData.ClientName}`} 
+          onChange={(e) => {
+              setFormData((prevData) => ({
+                  ...prevData,
+                  ClientName: e.target.value
+              }));
+          }}
+        />        
+        <label className="font-bold">Client Issue:</label>
+        <input value={formData.Issue} placeholder={`${data.Issue}`} 
+          onChange={(e) => {
+              setFormData((prevData) => ({
+                  ...prevData,
+                  Title: e.target.value
+              }));
+          }}
+        />
+        <label className="font-bold">Level of Access:</label>
+        <input value={formData.LOA} type="number" placeholder={`${formData.LOA}`} 
+          onChange={(e) => {
+              setFormData((prevData) => ({
+                  ...prevData,
+                  LOA: Math.max(parseInt(e.target.value), 0)
+              }));
+          }}
+        />
+        <label className="font-bold">Client Status:</label>
+        <input value={formData.ClientStatus} placeholder={formData.ClientStatus} 
+          onChange={(e) => {
+              setFormData((prevData) => ({
+                  ...prevData,
+                  ClientStatus: e.target.value
+              }));
+          }}
+        />
+        <label className="font-bold">New Result:</label>
+        <input value={formData.ClientStatus} placeholder={formData.Result} 
+          onChange={(e) => {
+              setFormData((prevData) => ({
+                  ...prevData,
+                  Result: e.target.value
+              }));
+          }}
+        />
+        <label className="font-bold mt-2 mb-2 border-b">Communication:</label>
+        <label className="font-bold">Email:</label>
+        <input value={formData.ClientStatus} placeholder={formData.Email} 
+          onChange={(e) => {
+              setFormData((prevData) => ({
+                  ...prevData,
+                  Email: e.target.value
+              }));
+          }}
+        />
+        <label className="font-bold">Phone Number:</label>
+        <input value={formData.ClientStatus} placeholder={formData.Phone} 
+          onChange={(e) => {
+              setFormData((prevData) => ({
+                  ...prevData,
+                  Phone: e.target.value
+              }));
+          }}
+        />
+        <label className="font-bold">Address:</label>
+        <input value={formData.ClientStatus} placeholder={formData.Address} 
+          onChange={(e) => {
+              setFormData((prevData) => ({
+                  ...prevData,
+                  Address: e.target.value
+              }));
+          }}
+        />
+        <button type="submit" className="bg-blue-600 text-white rounded shadow p-1 mt-2 hover:opacity-60">
+          Submit Adjustment
+        </button>
+      </form>
+    );    
+  }
+  
   return (
     <section className="p-5 border rounded-2xl overflow-hidden transition-all duration-500 ease-in-out flex flex-col justify-evenly w-full mt-10">
       <h2 className="text-lg font-bold">{data.Title} - {data.ClientName} - {data.AppointmentDate}</h2>
-      <input placeholder={`${data.Issue}`} />
-      <input placeholder={`LEVEL: ${data.LOA} Access`} />
-      <input placeholder={data.ClientStatus} />
+      <p>{data.Issue}</p>
+      <p>LEVEL:{data.LOA} Access`</p>
+      <p>{data.ClientStatus}</p>
       <p className={`${data.Result === "Pending" ? "animate-pulse" : "line-through"} bg-blue-400 border rounded p-2 w-[100px] text-center`}>
         {data.Result}
       </p>
+      <button onClick={() => setWantToAdjust(true)} className="bg-blue-600 text-white rounded shadow p-1 mt-2 hover:opacity-60">
+        Adjust Appointment?
+      </button>
     </section>
-  );
+  )
 };
 
 
